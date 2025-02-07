852400550d475fa94cb1b613864321d3752894aa
Ralph200607 <rjlinding@gmail.com>
2025-02-06 21:42:16 -0700

    2025-02-06

---
+++ Exercise2/TrainSystem/TrainSystem/Train.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TrainSystem
+{
+    public class Train
+    {
+        private readonly List<RailCar> _railCars = new List<RailCar>();
+
+        public Engine Engine { get; }
+
+        public int MaximumGrossWeight => Engine.HorsePower * 2000;
+
+        public int GrossWeight => Engine.Weight + _railCars.Sum(car => car.GrossWeight);
+
+        public IReadOnlyList<RailCar> RailCars => _railCars.AsReadOnly();
+
+        public int TotalCars => _railCars.Count;
+
+        public Train(Engine engine)
+        {
+            Engine = engine ?? throw new ArgumentNullException(nameof(engine), "Engine cannot be null.");
+        }
+
+        public void AddCar(RailCar car)
+        {
+            if (car == null)
+                throw new ArgumentNullException(nameof(car), "RailCar cannot be null.");
+
+            if (_railCars.Any(c => c.SerialNumber == car.SerialNumber))
+                throw new ArgumentException($"A RailCar with Serial Number {car.SerialNumber} already exists in the train.");
+
+            _railCars.Add(car);
+        }
+
+        public RailCar DetachCar(string serialNumber)
+        {
+            if (string.IsNullOrWhiteSpace(serialNumber))
+                throw new ArgumentNullException(nameof(serialNumber), "Serial number cannot be null or empty.");
+
+            RailCar carToRemove = _railCars.FirstOrDefault(c => c.SerialNumber == serialNumber);
+            if (carToRemove == null)
+                throw new ArgumentException($"RailCar with Serial Number {serialNumber} does not exist in the train.");
+
+            _railCars.Remove(carToRemove);
+            return carToRemove;
+        }
+    }
+}
+
---
+++ Exercise2/TrainSystem/TrainSystem/Utilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class Utilities
+{
+    public static bool IsPositiveNonZero(int value)
+    {
+        return value > 0;
+    }
+
+    public static bool InHundreds(int value)
+    {
+        return value % 100 == 0;
+    }
+}
