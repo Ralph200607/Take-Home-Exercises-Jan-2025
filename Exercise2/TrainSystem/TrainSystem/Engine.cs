36c06549c89abc9879310f4ab13d3dd6a96c270a
Ralph200607 <rjlinding@gmail.com>
2025-02-06 21:45:31 -0700

    Done some fixes, syntax

--- Exercise2/TrainSystem/TrainSystem/Engine.cs
+++ Exercise2/TrainSystem/TrainSystem/Engine.cs
@@ -20,7 +20,8 @@ namespace TrainSystem
             get => _weight;
             set
             {
-                if (InService) throw new Exception("Cannot modify weight while engine is in service.");
+                if (InService) 
+                    throw new Exception("Cannot modify weight while engine is in service.");
                 if (!Utilities.IsPositiveNonZero(value) || !Utilities.InHundreds(value))
                     throw new ArgumentException("Weight must be a positive, non-zero whole number in 100pound increments.");
                 _weight = value;
@@ -32,7 +33,8 @@ namespace TrainSystem
             get => _horsePower;
             set
             {
-                if (InService) throw new Exception("Cannot modify horsepower while engine is in service.");
+                if (InService) 
+                    throw new Exception("Cannot modify horsepower while engine is in service.");
                 if (value < 3500 || value > 5500 || value % 100 != 0)
                     throw new ArgumentException("Horsepower must be between 3500 and 5500 in 100 HP increments.");
                 _horsePower = value;
@@ -41,8 +43,10 @@ namespace TrainSystem
 
         public Engine(string model, string serialNumber, int weight, int horsePower)
         {
-            if (string.IsNullOrWhiteSpace(model)) throw new ArgumentNullException(nameof(model), "Model cannot be empty.");
-            if (string.IsNullOrWhiteSpace(serialNumber)) throw new ArgumentNullException(nameof(serialNumber), "Serial Number cannot be empty.");
+            if (string.IsNullOrWhiteSpace(model)) 
+                throw new ArgumentNullException(nameof(model), "Model cannot be empty.");
+            if (string.IsNullOrWhiteSpace(serialNumber)) 
+                throw new ArgumentNullException(nameof(serialNumber), "Serial Number cannot be empty.");
             if (!Utilities.IsPositiveNonZero(weight) || !Utilities.InHundreds(weight))
                 throw new ArgumentException("Weight must be a positive, non-zero whole number in 100-pound increments.");
             if (horsePower < 3500 || horsePower > 5500 || horsePower % 100 != 0)
